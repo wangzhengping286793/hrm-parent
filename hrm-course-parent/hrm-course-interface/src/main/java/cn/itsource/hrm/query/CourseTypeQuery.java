6382c973de750ece47f132ce17a6a8574cf0f545
wzp <root>
2019-12-26 16:24:25 +0800

    ww

---
+++ hrm-course-parent/hrm-course-interface/src/main/java/cn/itsource/hrm/query/CourseTypeQuery.java
@@ -0,0 +1,16 @@
+package cn.itsource.hrm.query;
+
+
+import cn.itsource.basic.query.BaseQuery;
+
+/**
+ * <p>
+ * 课程目录 查询参数对象
+ * </p>
+ *
+ * @author solargen
+ * @since 2019-12-26
+ */
+public class CourseTypeQuery extends BaseQuery {
+
+}
--- hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/CourseServiceApplication.java
+++ hrm-course-parent/hrm-course-service/src/main/java/cn/itsource/hrm/CourseServiceApplication.java
@@ -1,4 +1,19 @@
 package cn.itsource.hrm;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@MapperScan("cn.itsource.hrm.mapper")
+@EnableTransactionManagement
+@EnableEurekaClient
+@EnableSwagger2 //开启swagger
 public class CourseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CourseServiceApplication.class,args);
+    }
 }
